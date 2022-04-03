44d8bfc56b5f27b146c1aa910fabb06217e3a93a
Diego Ballesteros <diballesterosc@gmail.com>
2022-04-03 12:35:59 -0500

    fix: use actual callback

--- src/useKonamiCode.js
+++ src/useKonamiCode.js
@@ -3,12 +3,11 @@ import { useState, useEffect } from 'react';
 const KONAMI_CODE = '38,38,40,40,37,39,37,39,66,65';
 const KONAMI_KEYS = [38, 40, 37, 39, 66, 65];
 
-export default function useKonamiCode({ onKonamiCode }) {
+export default function useKonamiCode(onKonamiCode) {
 	const [code, setCode] = useState([]);
 	const [isKonamiCode, setIsKonamiCode] = useState(false);
 
 	useEffect(() => {
-		console.log(code);
 		if (code?.join(',') === KONAMI_CODE) {
 			setIsKonamiCode(true);
 			setCode([]);
@@ -16,13 +15,19 @@ export default function useKonamiCode({ onKonamiCode }) {
 		}
 	}, [code]);
 
+	useEffect(() => {
+		if (isKonamiCode) {
+			onKonamiCode();
+			setIsKonamiCode(false);
+		}
+	}, [isKonamiCode]);
+
 	const handleKeyDown = (e) => {
-		console.log(e.keyCode);
 		if (!KONAMI_KEYS.includes(e.keyCode)) {
 			setCode([]);
 			return;
 		}
-		setCode([...code, e.keyCode]);
+		setCode((prev) => [...prev, e.keyCode]);
 	};
 
 	useEffect(() => {
